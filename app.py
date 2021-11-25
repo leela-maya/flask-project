02dddf5ede1a006687e7c17c9641d08908e7892c
Leelavathi Maya <lmaya@innominds.com>
2021-11-25 16:26:05 +0530

    landing page updated

--- app.py
+++ app.py
@@ -6,13 +6,17 @@ app = Flask(__name__)
 def login():
     return render_template("login.html")
 
-@app.route("/")
+@app.route("/signup")
 def signup():
     return render_template("signup.html")
 
 @app.route("/forgotpassword")
 def fpassword():
     return render_template("forgotpassword.html")
+
+@app.route("/")
+def landingpage():
+    return render_template("landingpage.html")
 
 
 if __name__=='__main__':
